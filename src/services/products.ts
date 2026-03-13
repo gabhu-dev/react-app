e7b5803925a5b53d2dfea094015fe16906a7a3c6
gabhu-dev <gabhu@hotmail.es>
2026-03-12 21:08:16 -0500

    [add] APP_URL for constants

--- src/services/products.ts
+++ src/services/products.ts
@@ -1,4 +1,11 @@
-class ProductsService { }
+import { APP_URL } from '../constants'
+
+class ProductsService {
+  getProducts() {
+    return fetch(`${APP_URL}/products`)
+      .then(res => res.json())
+  }
+ }
 
 const productsService = new ProductsService()
 
