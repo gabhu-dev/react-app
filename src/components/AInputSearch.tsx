672750668e39acd9f3c19767649058611f892049
gabhu-dev <gabhu@hotmail.es>
2026-03-13 01:09:02 -0500

    [add] input search

--- src/components/AInputSearch.tsx
+++ src/components/AInputSearch.tsx
@@ -1,10 +1,17 @@
+import styles from './AInputSearch.module.css'
+import { Search } from 'lucide-react'
+
 function AInputSearch({ value, onChange }: { value: string, onChange: (v: string) => void }) {
   return (
-    <input
-      value={value}
-      onChange={(e) => onChange(e.target.value)}
-      placeholder="Buscar"
-    />
+    <div className={styles['search']}>
+      <Search className={styles['search__icon']} />
+      <input
+        className={styles['search__field']}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder="Buscar por nombre..."
+      />
+    </div>
   )
 }
 
--- src/components/ANavbar.tsx
+++ src/components/ANavbar.tsx
@@ -1,15 +1,31 @@
 import styles from './ANavbar.module.css'
+import { ShoppingCart, Heart } from 'lucide-react'
+import AInputSearch from './AInputSearch'
+import { useState, useEffect } from 'react'
+import { useAppDispatch } from '../store/hooks'
+import { getProducts } from '../store/slices/products'
+
 
 function ANavbar() {
+  const [search, setSearch] = useState('')
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(getProducts({ search, page: 1 }))
+  }, [dispatch, search])
   return (
     <nav className={styles.navbar}>
-      <ul>
+      <span>APP SHOP</span>
+      <AInputSearch value={search} onChange={setSearch} />
+      <ul className={styles['navbar__icons']}>
         <li>
-          <a href="/">Home</a>
+          <ShoppingCart size={24} />
         </li>
         <li>
-          <a href="/details">Details</a>
+          <Heart size={24} />
         </li>
+
+        <img className={styles['navbar__avatar']} src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="avatar" />
       </ul>
     </nav>
   )
